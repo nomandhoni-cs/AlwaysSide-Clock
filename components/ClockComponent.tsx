83551e7f70a6ab2257de654ca09637a7f7652736
Noman Dhoni <nomandhoni@yahoo.com>
2024-10-08 15:34:44 +0600

    Git push

--- components/ClockComponent.tsx
+++ components/ClockComponent.tsx
@@ -9,6 +9,7 @@ const ClockComponent: React.FC<ClockComponentProps> = ({
   currentTime,
   mainClockName
 }) => {
+  // console.log(currentTime)
   return (
     <div className="flex flex-col items-start justify-center w-full h-24 bg-white border border-gray-200 rounded-lg shadow-sm p-4">
       <h2 className="text-sm font-semibold text-gray-600 mb-1">
